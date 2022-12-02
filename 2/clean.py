c6844cc3b98791c19ba30c49930db0f35bfc5b2a
WizzyGeek <ojasscoding@gmail.com>
2022-12-02 22:44:06 +0530

    first commit

---
+++ 2/clean.py
@@ -0,0 +1,1 @@
+print((lambda inp: [sum(win(p, r) for p, r in zip(map(lambda s: ord(s) - 64, inp[::2]), map(lambda s: ord(s) - 87, inp[1::2]))) for win in ((lambda p, r: (((r-2) % 3 + 1) == p)*6 + (r == p)*3 + r),(lambda p, r: ((r == 1) * (((p + 1) % 3) + 1)) or ((r == 2) * (p + 3)) or ((r == 3) * ((p % 3 + 1) + 6))))])(open("input.txt", "r").read().split()))
---
+++ README.md
@@ -0,0 +1,3 @@
+I dunno man, I am midcore dev
+
+please send PR for fast C++ code so that it looks good
