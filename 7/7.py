865293532219463bc55ae9c2ab2df31c4c121e30
WizzyGeek <ojasscoding@gmail.com>
2022-12-07 22:10:31 +0530

    I got baited by unique file names.....

--- 7/7.py
+++ 7/7.py
@@ -1,65 +1,51 @@
-# No repl first day
-g = list(filter(bool, open("7/example.txt", "r").read().split("\n")))
-# print(g)
+g = list(filter(bool, open("7/input.txt", "r").read().split("\n")))
 
-cd = ""
-tree: dict[str, list[int | list[str]]] = {}
-stack: list[str] = []
+tree = {}
+cp = [] # current path
 c = 0
-# print(g)
-crd: list[int | list[str]] = [0, []]
-while c < len(g):  # type: ignore
-    i = g[c]
-    if i == "$ cd ..":
-        cd = stack.pop()
-    elif i.startswith("$ cd "):
-        stack.append(cd)
-        cd = i[5:]
-        tree.setdefault(cd, [0, []])
-    elif i == "$ ls":
-        c += 1
-        i = g[c]
-        crd = tree[cd]
-        while i[0] != "$":
-            p = i.split()
-            if i.startswith("dir"):
-                n = p[1]
-                crd[1].append(n)  # type: ignore
-                tree.setdefault(n, [0, []])
-            else:
-                # print(c,p[0])
-                crd[0] += int(p[0]) # type: ignore
-            c += 1
-            try:
-                i = g[c]
-            except:
+b = len(g)
+while c < b:
+    cmd = g[c]
+    # print(cp, cmd)
+    if cmd.startswith("$ cd "):
+        n = cmd[5:]
+        if n != "..":
+            cp.append(cmd[5:])
+            tree.setdefault(tuple(cp), 0) # Our problem has entirely to do with size
+        else:
+            cp.pop()
+    elif cmd.startswith("$ ls"):
+        c_o1 = g[c+1]
+        while not c_o1.startswith("$"):
+            if not c_o1.startswith("dir"):
+                # new file, find all previous directories and update size
+                s = int(c_o1.split()[0])
+                for i in range(1, len(cp) + 1): # current path
+                    tree[tuple(cp[:i])] += s # add to size
+            c += 1 # inc
+            if c >= b - 1: # all parsed?
                 break
-        c -= 1 # compensate one element look ahead for exit
-    else:
-        print(i);
-        raise ValueError
-
+            c_o1 = g[c+1] # update
+            # ignore dir, cp tracks the structure
     c += 1
 
-root = tree["/"]
-s = 0
-ds = root[1]
-
-f = {}
-
-stack = ["/"]
-cd = "/"
-while stack:
-    leaf = tree[cd]
-    if not leaf[1]:
-        tree[stack[-1]][0] += leaf[0]  # type: ignore
-        try:
-            cd = tree[stack[-1]][1].pop()
-        except:
-            cd = stack.pop()
-    else:
-        stack.append(cd)
-        cd = leaf[1].pop()
-
 print(tree)
-print(sum((i[0] < 100_000) * i[0] for i in tree.values()))
+
+sm = 0
+for k in tree.values():
+    if k <= 100000:
+        sm += k
+
+print(sm)
+
+space = 70000000
+needed = 30000000
+free = 70000000 - tree[("/",)]
+print(free, needed, free + 6400111)
+
+m = float("inf")
+for k in tree.values():
+    if (free + k) >= needed:
+        m = min(m, k)
+
+print(m)
